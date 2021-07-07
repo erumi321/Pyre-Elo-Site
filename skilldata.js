d0159dab9bda37f5f962e1bfab872276ef284cd3
erumi321 <42208651+erumi321@users.noreply.github.com>
2021-07-07 19:35:08 -0400

    rework match history and add history page

---
+++ skilldata.js
@@ -0,0 +1,480 @@
+var skillData = {
+    LosingScoreSkill5 : {
+        Icon : "skill_icon_pyredamage",
+        Name : "Hex of Defeat"
+    },
+    CastMonsterSkill : {
+        Icon : "skill_icon_cast",
+        Name : "Greater Blast"
+    },
+    ChargeMonsterSkill : {
+        Icon : "skill_icon_aura",
+        Name : "Sudden Blast"
+    },
+    LosingScoreScoreDebuff : {
+        Icon : "skill_icon_special",
+        Name : "Stubborn Flame"
+    },
+    WinningScoreSkill5 : {
+        Icon : "skill_icon_pyredamage",
+        Name : "Hex of Victory"
+    },
+    MonsterJumpSkill : {
+        Icon : "skill_icon_stamina",
+        Name : "Vital Pounce"
+    },
+    MonsterSprintSkill : {
+        Icon : "skill_icon_speed",
+        Name : "Serpent Swiftness"
+    },
+    TauntPolymorphSide : {
+        Icon : "skill_icon_taunt",
+        Name : "Feral Curse"
+    },
+    DoubleJump : {
+        Icon : "skill_icon_evade",
+        Name : "Cloud Jump"
+    },
+    SprintSpeed : {
+        Icon : "skill_icon_sprint",
+        Name : "Lightning Run"
+    },
+    InstantRespawn : {
+        Icon : "skill_icon_respawn",
+        Name : "Lucky Break"
+    },
+    TripleJump : {
+        Icon : "skill_icon_evade",
+        Name : "Moon Sault"
+    },
+    SacrificeScoreSkill5 : {
+        Icon : "skill_icon_pyredamage",
+        Name : "Glory Dive"
+    },
+    UnlimitedRange : {
+        Icon : "skill_icon_cast",
+        Name : "Keen Eye"
+    },
+    AuraKillDetonate100 : {
+        Icon : "skill_icon_banishment",
+        Name : "Explosive Temper"
+    },
+    SacrificeScoreRespawn100 : {
+        Icon : "skill_icon_special",
+        Name : "Guiding Light"
+    },
+    DoubleRush : {
+        Icon : "skill_icon_sprint",
+        Name : "Long Stride"
+    },
+    EvadeStun : {
+        Icon : "skill_icon_evade",
+        Name : "Crushing Heel"
+    },
+    TauntScoreSkill10 : {
+        Icon : "skill_icon_taunt",
+        Name : "Brazen Manner"
+    },
+    AuraSpikeKill : {
+        Icon : "skill_icon_ball",
+        Name : "Celestial Spike"
+    },
+    KillsRegenerateStamina100 : {
+        Icon : "skill_icon_stamina",
+        Name : "Relentless Vigor"
+    },
+    PlusPresenceSkill : {
+        Icon : "skill_icon_aura",
+        Name : "Fierce Presence"
+    },
+    PyreHealthSkill35 : {
+        Icon : "skill_icon_pyrehealth",
+        Name : "Enduring Flame"
+    },
+    PlusEnemyRespawnTime : {
+        Icon : "skill_icon_banishment",
+        Name : "Greater Banishment"
+    },
+    AirSpeed : {
+        Icon : "skill_icon_evade",
+        Name : "Swift Flight"
+    },
+    DashCooldown : {
+        Icon : "skill_icon_sprint",
+        Name : "Shrike Dash"
+    },
+    TauntSwap : {
+        Icon : "skill_icon_taunt",
+        Name : "Sleight of Wing"
+    },
+    PlusSpeedSkill : {
+        Icon : "skill_icon_speed",
+        Name : "Greater Celerity"
+    },
+    OnKillBuffSpeed : {
+        Icon : "skill_icon_banishment",
+        Name : "Winged Fury"
+    },
+    CastExplosion : {
+        Icon : "skill_icon_cast",
+        Name : "Fell Swoop"
+    },
+    RespawnBuff : {
+        Icon : "skill_icon_respawn",
+        Name : "Relentless Pursuit"
+    },
+    PlusStatsSkill : {
+        Icon : "skill_icon_special",
+        Name : "Natural Superiority"
+    },
+    MoveBetter : {
+        Icon : "skill_icon_speed",
+        Name : "Elusive Nature"
+    },
+    CastFastRespawn : {
+        Icon : "skill_icon_respawn",
+        Name : "Safe Return"
+    },
+    ExplodeBigger : {
+        Icon : "skill_icon_banishment",
+        Name : "Titans' Rage"
+    },
+    AnyScoreSkill : {
+        Icon : "skill_icon_pyredamage",
+        Name : "Inner Glory"
+    },
+    Beacon : {
+        Icon : "skill_icon_special",
+        Name : "Moon Sign"
+    },
+    OnDeathExplosion : {
+        Icon : "skill_icon_aura",
+        Name : "Last Laugh"
+    },
+    TauntFeralImp : {
+        Icon : "skill_icon_taunt",
+        Name : "Wild Heart"
+    },
+    BetterBeacon : {
+        Icon : "skill_icon_special",
+        Name : "Star Sign"
+    },
+    MobilityChargeTime : {
+        Icon : "skill_icon_speed",
+        Name : "Heightened Reflex"
+    },
+    JumpDistance : {
+        Icon : "skill_icon_evade",
+        Name : "Traitor's Flight"
+    },
+    SprintAreaSlow : {
+        Icon : "skill_icon_sprint",
+        Name : "Webbed Feet"
+    },
+    JumpRespawn : {
+        Icon : "skill_icon_respawn",
+        Name : "False Step"
+    },
+    CastChargeTime : {
+        Icon : "skill_icon_cast",
+        Name : "Snap Cast"
+    },
+    TossSkillMediumAlt : {
+        Icon : "skill_icon_ball",
+        Name : "Quick Fling"
+    },
+    TauntBallTeleport : {
+        Icon : "skill_icon_taunt",
+        Name : "Sudden Grasp"
+    },
+    UnlimitedBallStamina : {
+        Icon : "skill_icon_special",
+        Name : "Dark Vigor"
+    },
+    StaminaSkill : {
+        Icon : "skill_icon_speed",
+        Name : "Martial Training"
+    },
+    MassivePowerShot : {
+        Icon : "skill_icon_cast",
+        Name : "Critical Strike"
+    },
+    JumpKillSkill : {
+        Icon : "skill_icon_evade",
+        Name : "Shoulder Smash"
+    },
+    GoalLifeStealSkill50 : {
+        Icon : "skill_icon_pyrehealth",
+        Name : "Burning Resolve"
+    },
+    TeamStaminaRechargeSkill : {
+        Icon : "skill_icon_stamina",
+        Name : "Shared Tenacity"
+    },
+    TeamBallSkill : {
+        Icon : "skill_icon_ball",
+        Name : "Moon Formation"
+    },
+    TeamReviveSkill : {
+        Icon : "skill_icon_respawn",
+        Name : "Sacred Bond"
+    },
+    TeamEnemyBanishTime : {
+        Icon : "skill_icon_banishment",
+        Name : "Divine Retribution"
+    },
+    SpawnDefenderUnit : {
+        Icon : "skill_icon_special",
+        Name : "Guardian Sapling"
+    },
+    DefenderAura : {
+        Icon : "skill_icon_aura",
+        Name : "Sturdy Sapling"
+    },
+    TauntSaplingExplode : {
+        Icon : "skill_icon_cast",
+        Name : "Unstable Sapling"
+    },
+    TeamHeadWinds : {
+        Icon : "skill_icon_banishment",
+        Name : "Numbing Gust"
+    },
+    PyreLastHitShield10 : {
+        Icon : "skill_icon_pyrehealth",
+        Name : "Final Kindling"
+    },
+    ShieldAllies : {
+        Icon : "skill_icon_evade",
+        Name : "Guardian Shield"
+    },
+    PlusHopeSkill : {
+        Icon : "skill_icon_respawn",
+        Name : "Steadfast Hope"
+    },
+    PyreLastHitShield40 : {
+        Icon : "skill_icon_pyrehealth",
+        Name : "Miracle Kindling"
+    },
+    AllDeadRespawn : {
+        Icon : "skill_icon_respawn",
+        Name : "Valiant Return"
+    },
+    TauntTimeout : {
+        Icon : "skill_icon_taunt",
+        Name : "Stunning Claim"
+    },
+    SoleUnitPowerUp : {
+        Icon : "skill_icon_speed",
+        Name : "Heroic Stand"
+    },
+    BanishScoreBonus : {
+        Icon : "skill_icon_pyredamage",
+        Name : "Seized Chance"
+    },
+    KillsRespawnFriends100 : {
+        Icon : "skill_icon_aura",
+        Name : "Avenging Aid"
+    },
+    WeaponReTeleport : {
+        Icon : "skill_icon_cast",
+        Name : "Quick Draw"
+    },
+    TrailExplosion : {
+        Icon : "skill_icon_banishment",
+        Name : "Greater Cleave"
+    },
+    WeaponRechargeStamina : {
+        Icon : "skill_icon_stamina",
+        Name : "Vigorous Slash"
+    }
+}
+
+var talismanData = {
+    TossDistanceItem : {
+        Icon : "sling_bullet_01",
+        Name : "Sling Bullet"
+    },
+    
+    SacrificeScoreRespawnItem : {
+        Icon : "prayer_beads_01",
+        Name : "Prayer Beads"
+    },
+    PowerUpDropOnKillItem : {
+        Icon : "faerie_dust_01",
+        Name : "Faerie Spirit"
+    },
+    JumpBashItem : {
+        Icon : "titan_tooth_01",
+        Name : "Titan Tooth"
+    },
+    TossChargeTimeItem : {
+        Icon : "star_splinter_01",
+        Name : "Star Splinter"
+    },
+    StaminaOnBallPickupItem : {
+        Icon : "luminous_idol_01",
+        Name : "Luminous Idol"
+    },
+    PyrePreDamageItem : {
+        Icon : "frozen_soul_01",
+        Name : "Frozen Soul"
+    },
+    PassOnDeathItemMascot : {
+        Icon : "bright_wisp_01",
+        Name : "Bright Wisp"
+    },
+    OnDeathExplosionItem : {
+        Icon : "vengeful_vow_01",
+        Name : "Vengeful Vow"
+    },
+    KillsRespawnFriendsItemDoc : {
+        Icon : "webbed_lanthorn_01",
+        Name : "Webbed Lanthorn"
+    },
+    KillsRegenerateStaminaItemDoc : {
+        Icon : "nihiland_leech_01",
+        Name : "Nihiland Leech"
+    },
+    WinningScoreItemDiva : {
+        Icon : "living_flame_01",
+        Name : "Living Flame"
+    },
+    LosingScoreItemDiva : {
+        Icon : "dying_flame_01",
+        Name : "Dying Flame"
+    },
+    PyreDamageItem : {
+        Icon : "astral_eye_01",
+        Name : "Astral Eye"
+    },
+
+    QuickRespawnItem : {
+        Icon : "moon_crest_01",
+        Name : "Moon Crest",
+    },
+    SacrificeDamageItem : {
+        Icon : "cur_fang_01",
+        Name : "Cur Fang",
+    },
+    PyreHealthItem : {
+        Icon : "ashen_coal_01",
+        Name : "Ashen Coal"
+    },
+    CarrySpeedItem : {
+        Icon : "lunar_glass_01",
+        Name : "Lunar Glass"
+    },
+    PlusRespawnItem : {
+        Icon : "faith_stone_01",
+        Name : "Faith Stone"
+    },
+    PlusAuraItem : {
+        Icon : "scribe_rock_01",
+        Name : "Scribe Rock"
+    },
+    AuraCastRangeItem : {
+        Icon : "thorned_knot_01",
+        Name : "Thorned Knot"
+    },
+    GoalHealItem : {
+        Icon : "righteous_flame_01",
+        Name : "Righteous Flame"
+    },
+    EnemyRespawnIncreaseItem : {
+        Icon : "sunken_shadow_01",
+        Name : "Sunken Shadow"
+    },
+    PyreLastHitShieldItem : {
+        Icon : "rite_lite_01",
+        Name : "Rite Light"
+    },
+    ThrowDamageItem : {
+        Icon : "twilight_shard",
+        Name : "Twilight Shard"
+    },
+    KillDetonateItem : {
+        Icon : "infernal_bloom_01",
+        Name : "Infernal Bloom"
+    },
+    SetItem01 : {
+        Icon : "jeweled_band_01",
+        Name : "Jeweled Band"
+    },
+    SetItem02 : {
+        Icon : "runed_band_01",
+        Name : "Runed Band"
+    },
+    RespawnWeaponItem : {
+        Icon : "bottled_void_01",
+        Name : "Bottled Void"
+    },
+    CursedItem01 : {
+        Icon : "black_hoof_01",
+        Name : "Black Hoof"
+    },
+    CursedItem02 : {
+        Icon : "black_heart_01",
+        Name : "Black Heart"
+    },
+    CursedItem03 : {
+        Icon : "black_claw_01",
+        Name : "Black Claw"
+    },
+    GoalLifeStealItem : {
+        Icon : "flame_leech_01",
+        Name : "Flame Leech"
+    },
+    TeamHeadwindsItem : {
+        Icon : "typhoon_bottle_01",
+        Name : "Typhoon Bottle"
+    },
+    AuraSpikeKillItem : {
+        Icon : "shooting_star_01",
+        Name : "Shooting Star"
+    },
+    UniqueSmallItem01 : {
+        Icon : "jomuers_fang_01",
+        Name : "Jomuer's Fang"
+    },
+    UniqueMediumItem01 : {
+        Icon : "gols_bracer_01",
+        Name : "Gol's Bracer"
+    },
+    UniqueLargeItem01 : {
+        Icon : "soliams_horn_01",
+        Name : "Soliam's Horn"
+    },
+    UniqueMediumAltItem01 : {
+        Icon : "khaylmers_anklet_01",
+        Name : "Khaylmer's Anklet"
+    },
+    UniqueImpItem01 : {
+        Icon : "haubs_wing_01",
+        Name : "Ha'ub's Wing"
+    },
+    UniqueTrailItem01 : {
+        Icon : "ores_scale_01",
+        Name : "Ore's Scale"
+    },
+    UniqueFlyingItem01 : {
+        Icon : "_triestas_plume_01",
+        Name : "Triesta's Plume"
+    },
+    UniqueTreeItem01 : {
+        Icon : "lus_reed_01",
+        Name : "Lu's Bough"
+    },
+    UniqueMonsterItem01 : {
+        Icon : "milithes_skin_01",
+        Name : "Milithe's Tail"
+    },
+    PlusSpeedItem : {
+        Icon : "tailwind_crest_01",
+        name : "Tailwind Crest"
+    },
+    TeamStaminaRechargeItem : {
+        Icon : "mottled_flask_01",
+        Name : "Mottled Flask"
+    },
+
+}
